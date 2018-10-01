d57ed99a94263b69fe09eacd777039ca043e96f5
Stephen Bush <darkshark_007@yahoo.com>
2018-10-01 17:08:01 -0600

    master Init

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:GeneticAlgorithmEngine/GeneticAlgorithmEngine.dart' as GeneticAlgorithmEngine;
+
+main(List<String> arguments) {
+  print('Hello world: ${GeneticAlgorithmEngine.calculate()}!');
+}
---
+++ test/GeneticAlgorithmEngine_test.dart
@@ -0,0 +1,8 @@
+import 'package:GeneticAlgorithmEngine/GeneticAlgorithmEngine.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
